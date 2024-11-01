67d3b50d0971401e2dc6ae90d9a47484471ce6f2
zzy <1061342204@qq.com>
2024-11-02 00:51:17 +0800

    配置了dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+    },
+  },
+  plugins: [
+    vue(),
+    dts({ insertTypesEntry: true, copyDtsFiles: false }),
+  ],
 })
